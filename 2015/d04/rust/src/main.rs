f1f52de6c017bc553356953ccd71bb8d0220f85b
Steven Gillies <stevengillies@outlook.com>
2023-05-25 15:31:38 +0100

    2015 d04 rust pt2

--- 2015/d04/rust/src/main.rs
+++ 2015/d04/rust/src/main.rs
@@ -2,22 +2,27 @@ use md5;
 
 fn main() {
     p1();
+    p2();
 }
 
 fn p1() {
-    let ans = mine_coin("bgvyzdsv");
+    let ans = mine_coin("bgvyzdsv", "00000");
     println!("{}", ans);
 }
 
-fn mine_coin(key: &str) -> i32 {
+fn p2() {
+    let ans = mine_coin("bgvyzdsv", "000000");
+    println!("{}", ans);
+}
+
+fn mine_coin(key: &str, prefix: &str) -> i32 {
     let mut i = 0;
     loop {
-        println!("{}", i);
         let mut coin = key.to_string();
         coin.push_str(i.to_string().as_str());
         let digest = md5::compute(coin.as_bytes());
         let hash = format!("{:x}", digest);
-        if hash[0..5].eq("00000") {
+        if hash[0..prefix.len()].eq(prefix) {
             break;
         }
         i += 1;
@@ -31,7 +36,7 @@ mod tests {
 
     #[test]
     fn test_p1() {
-        assert_eq!(609043, mine_coin("abcdef"));
-        assert_eq!(1048970, mine_coin("pqrstuv"));
+        assert_eq!(609043, mine_coin("abcdef", "00000"));
+        assert_eq!(1048970, mine_coin("pqrstuv", "00000"));
     }
 }
